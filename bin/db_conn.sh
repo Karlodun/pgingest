44903bfa5a6a1da4bdcd23d044303dde46d7d95e
Karlodun <Mihail-Klr@gmx.net>
2020-05-06 18:06:06 +0200

    Update db_conn.sh

--- bin/db_conn.sh
+++ bin/db_conn.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # this should be moved into ~/.bashrc for productive or ~/.profile for tests
-# with export 
-db_conn="user=cbre_importer password=FrGged43vrfE host=de-microapps.csg2po3twyai.eu-west-2.rds.amazonaws.com dbname=cbre_importer"
+# with export
+# fill in <passwd>
+db_conn="user=cbre_importer password=<passwd> host=de-microapps.csg2po3twyai.eu-west-2.rds.amazonaws.com dbname=cbre_importer"
 g="grimoire"
