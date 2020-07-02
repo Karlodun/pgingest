518da6f8eb59b827d7e416d65d9f3e05dde31fcd
Karlodun <Mihail-Klr@gmx.net>
2020-07-02 22:38:26 +0200

    Update auto_importer.sh

--- bin/auto_importer.sh
+++ bin/auto_importer.sh
@@ -1,24 +1,15 @@
 #!/bin/bash
-my_path="`dirname \"$0\"`"              # relative
-my_path="`( cd \"$my_path\" && pwd )`"  # absolutized and normalized
-if [ -z "$my_path" ] ; then
-  # error; for some reason, the path is not accessible
-  # to the script (e.g. permissions re-evaled after suid)
-  exit 1  # fail
-fi
-source "$my_path/db_conn.sh"
 
-db_res=$(psql "$db_conn" -t -q -c "select import_name from grimoire.import_source WHERE autofetch AND (last_update+download_interval)<=now();")
-cd $my_path/../tmp/
+db_res=$(psql service=pgingest -c "select import_name from grimoire.import_source WHERE autofetch AND (last_update+download_interval)<=now();")
 
 for d in $db_res; do
-    mkdir $d
-    cd $d
-    $( ../../bin/import.sh $d)
+    ~pgingest/bin/import.sh $d
     # "updating timers for $d"
-    psql "$db_conn" -t -q -c "UPDATE grimoire.import_source SET last_update = now() WHERE import_name= '$d';
+    psql service=pgingest -c "UPDATE grimoire.import_source SET last_update = now() WHERE import_name= '$d';
     UPDATE grimoire.task_board SET status = 'completed' WHERE import_name= '$d';"
     # elaborate: run tasks in target DB automagically from here if auto fetch is on
+    # dynamic code view for table recreation or table filling should check if the error reports are empty
+    # if yes: run, if no: error.
     cd ..
     rm -r $d
 done
