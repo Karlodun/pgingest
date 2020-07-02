518f58d4e5fc84a78713008a4807fb0595a211ba
Karlodun <Mihail-Klr@gmx.net>
2020-07-02 22:37:53 +0200

    Delete db_conn.sh

--- bin/db_conn.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# this should be moved into ~/.bashrc for productive or ~/.profile for tests
-# with export
-# fill in <passwd>
-db_conn="user=cbre_importer password=<passwd> host=de-microapps.csg2po3twyai.eu-west-2.rds.amazonaws.com dbname=cbre_importer"
-g="grimoire"
